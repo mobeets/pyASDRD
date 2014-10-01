62bd1aaa87c959401e7b54281ea040a1dfe808b4
mobeets <mobeets@gmail.com>
2014-10-01 12:45:14 -0500

    ARD seems to be working at least

--- asdard.py
+++ asdard.py
@@ -1,38 +1,39 @@
 import numpy as np
 import scipy.optimize
 
-def ASDReg(ro, D, delta_sq):
-    return np.exp(-ro-0.5*D/(delta_sq**2))
 
-def ASDPostCov(RegInv, XX, sigma_sq):
+def PostCov(RegInv, XX, sigma_sq):
     return np.linalg.inv((XX/sigma_sq) + RegInv)
 
-def ASDPostMean(PostCov, XY, sigma_sq):
-    return PostCov.dot(XY)/sigma_sq
+def PostMean(sigma, XY, sigma_sq):
+    return sigma.dot(XY)/sigma_sq
+
+def ASDReg(ro, D, delta_sq):
+    return np.exp(-ro-0.5*D/(delta_sq**2))
 
 def ASDEviGradient((ro, delta_s, sigma_sq), X, Y, XX, XY, p, q, D):
     """
     gradient of log evidence w.r.t. hyperparameters
     """
     # update regularizer
-    S = ASDReg(ro, D, delta_s)
-    S_inv = np.linalg.inv(S)
+    Reg = ASDReg(ro, D, delta_s)
+    RegInv = np.linalg.inv(Reg)
 
     # posterior cov and mean
-    sigma =  ASDPostCov(S_inv, XX, sigma_sq)
-    mu = ASDPostMean(sigma, XY, sigma_sq)
+    sigma =  PostCov(RegInv, XX, sigma_sq)
+    mu = PostMean(sigma, XY, sigma_sq)
     err = Y - X.dot(mu)
     sse = err.dot(err.T)
     
     # hyperparameter derivatives
-    Z = (S - sigma - mu.dot(mu.T)) * S_inv
-    v = -p + np.trace(np.eye(q) - sigma*S_inv)
+    Z = (Reg - sigma - mu.dot(mu.T)) * RegInv
+    v = -p + np.trace(np.eye(q) - sigma*RegInv)
     # Z is too big after first iteration because mu.dot(mu.T) overpowers...
     der_ro = 0.5*np.trace(Z)
-    der_delta_s = -0.5*np.trace(Z * np.multiply(S, D/(delta_s**3)) * S_inv)
+    der_delta_s = -0.5*np.trace(Z * np.multiply(Reg, D/(delta_s**3)) * RegInv)
     der_sigma_sq = sse/(sigma_sq**2) + v/sigma_sq
 
-    return (der_ro, der_delta_s, der_sigma_sq), (mu, sigma, S, S_inv, sse)
+    return (der_ro, der_delta_s, der_sigma_sq), (mu, sigma, Reg, RegInv, sse)
 
 logDet = lambda x: np.linalg.slogdet(x)[1]
 def ASDEvi(X, Y, Reg, sigma, sigma_sq, p, q):
@@ -54,7 +55,7 @@ def ASD(X, Y, D):
 
     Returns:
         w - (q x 1) the regularized solution
-        S - (q x q) the ASD regularizer
+        Reg - (q x q) the ASD regularizer
     
     Implelements the ASD regression descrived in:
         M. Sahani and J. F. Linden.
@@ -65,8 +66,8 @@ def ASD(X, Y, D):
     def objfun_maker(X, Y, XX, XY, p, q, D):
         def objfcn((ro, delta_s, sigma_sq)):
             hyper = (ro, delta_s, sigma_sq)
-            der_hyper, (mu, sigma, S, S_inv, sse) = ASDEviGradient(hyper, X, Y, XX, XY, p, q, D)
-            evi = ASDEvi(X, Y, S, sigma, sigma_sq, p, q)
+            der_hyper, (mu, sigma, Reg, RegInv, sse) = ASDEviGradient(hyper, X, Y, XX, XY, p, q, D)
+            evi = ASDEvi(X, Y, Reg, sigma, sigma_sq, p, q)
             return evi, -np.array(der_hyper)
         return objfcn
 
@@ -81,13 +82,13 @@ def ASD(X, Y, D):
     theta0 = (ro, delta_s, sigma_sq)
 
     objfcn = objfun_maker(X, Y, XX, XY, p, q, D)
-    theta = scipy.optimize.minimize(objfcn, theta0, method='L-BFGS-B', jac=True)
+    theta = scipy.optimize.minimize(objfcn, theta0, method='TNC', jac=True)
     print theta
-    der_hyper, (mu, sigma, S, S_inv, sse) = ASDEviGradient(theta['x'], X, Y, XX, XY, p, q, D)
+    der_hyper, (mu, sigma, Reg, RegInv, sse) = ASDEviGradient(theta['x'], X, Y, XX, XY, p, q, D)
 
-    return mu, S
+    return mu, Reg
 
-def ASD_FP(X, Y, D):
+def ASD_FP(X, Y, D, niters=100):
     """
     X - (p x q) matrix with inputs in rows
     Y - (p, 1) matrix with measurements
@@ -95,7 +96,7 @@ def ASD_FP(X, Y, D):
 
     Returns:
         w - (q x 1) the regularized solution
-        S - (q x q) the ASD regularizer
+        Reg - (q x q) the ASD regularizer
     
     Implelements the ASD regression descrived in:
         M. Sahani and J. F. Linden.
@@ -117,11 +118,11 @@ def ASD_FP(X, Y, D):
     
     step = 0.01
     print 'i, ro, delta_s, sigma_sq, der_ro_m, der_delta_s_m, step'
-    for i in xrange(100):
+    for i in xrange(niters):
         print i, ro, delta_s, sigma_sq, der_ro_m, der_delta_s_m, step, der_ro
         
         hyper = (ro, delta_s, sigma_sq)
-        der_hyper, (mu, sigma, S, S_inv, sse) = ASDEviGradient(hyper, X, Y, XX, XY, p, q, D)
+        der_hyper, (mu, sigma, Reg, RegInv, sse) = ASDEviGradient(hyper, X, Y, XX, XY, p, q, D)
         (der_ro, der_delta_s, der_sigma_sq) = der_hyper
         
         if der_ro_m*der_ro + der_delta_s_m*der_delta_s < 0:
@@ -136,14 +137,14 @@ def ASD_FP(X, Y, D):
         ro += step * der_ro_m
         delta_s += step * der_delta_s_m
         delta_s = np.max([0.5, np.abs(delta_s)])
-        v = -p + np.trace(np.eye(q) - sigma*S_inv)
+        v = -p + np.trace(np.eye(q) - sigma*RegInv)
         sigma_sq = -sse/v
 
     hyper = (ro, delta_s, sigma_sq)
-    der_hyper, (mu, sigma, S, S_inv, sse) = ASDHyperGradient(hyper, X, Y, XX, XY, p, q, D)
-    return mu, S
+    der_hyper, (mu, sigma, Reg, RegInv, sse) = ASDHyperGradient(hyper, X, Y, XX, XY, p, q, D)
+    return mu, Reg
 
-def ARD(X, Y):
+def ARD(X, Y, niters=100):
     """
     X - (p x q) matrix with inputs in rows
     Y - (p, 1) matrix with measurements
@@ -154,25 +155,28 @@ def ARD(X, Y):
         In S. Becker, S. Thrun, and K. Obermayer, eds., Advances in Neural Information Processing Systems, vol. 15, pp. 301-308, Cambridge, MA, 2003. 
     """
     (p,q) = np.shape(X)
+    XX = X.T.dot(X)
+    XY = X.T.dot(Y)
     
-    #initialize parameters
-    sigma_sq = 0.1
-    CC = X.T * X
-    XY = X.T * Y
-    start_flag = False
-        
-    alpha = np.mat(np.zeros((q,1)))+2.0
-    
-    for i in xrange(0,100):
-        sigma = np.linalg.inv(CC/sigma_sq + np.diagflat(alpha)) 
-        ni = sigma * (XY) /  (sigma_sq)
-        sigma_sq = np.sum(np.power(Y - X*ni,2))/(p - np.sum(1 - np.multiply(np.mat(np.diagonal(sigma)).T,alpha)))
-        print np.min(np.abs(ni))
-        alpha =  np.mat(np.divide((1 - np.multiply(alpha,np.mat(np.diagonal(sigma)).T)) , np.power(ni,2)))
-        print  sigma_sq
-        
-    w = np.linalg.inv(CC + sigma_sq * np.diagflat(alpha)) * (XY)
-    return w
+    # initialize parameters
+    sigma_sq = 0.1        
+    alpha = 2*np.ones([q, 1])
+
+    for i in xrange(niters):
+        RegInv = np.diagflat(alpha)
+        sigma = PostCov(RegInv, XX, sigma_sq)
+        mu = PostMean(sigma, XY, sigma_sq)
+        err = Y - X.dot(mu)
+        sse = err.dot(err.T)
+        v = p - np.sum(1 - np.multiply(np.mat(np.diagonal(sigma)).T, alpha))
+        sigma_sq = np.sum(sse/v)
+        va = alpha.T.dot(np.diagonal(sigma))
+        alpha = np.divide((1 - va), np.power(mu, 2))
+
+    RegInv = np.diagflat(alpha)
+    sigma = PostCov(RegInv, XX, sigma_sq)
+    mu = PostMean(sigma, XY, sigma_sq)
+    return mu, RegInv
     
 def ASDRD(X, Y, S):
     """
@@ -187,7 +191,7 @@ def ASDRD(X, Y, S):
     D,V = np.linalg.eigh(S)
     V = np.mat(V)
     D = np.diag(np.sqrt(D))
-    R =  V*  D * V.T
-    w = ARD(X*R,Y)
+    R = V.dot(D).dot(V.T)
+    w = ARD(X.dot(R),Y)
     w = R * w
     return w
